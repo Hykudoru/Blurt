56b37f3cdf812309256ec3ff9b12dc1c0c6e2a86
Alexander Fish <alexanderfish7@gmail.com>
2017-10-15 02:27:25 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace Blurt_Reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSpeak = new System.Windows.Forms.Button();
+            this.labelVolume = new System.Windows.Forms.Label();
+            this.labelSpeed = new System.Windows.Forms.Label();
+            this.trackBarVolume = new System.Windows.Forms.TrackBar();
+            this.trackBarSpeed = new System.Windows.Forms.TrackBar();
+            this.radioButtonMale = new System.Windows.Forms.RadioButton();
+            this.radioButtonFemale = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelVoiceControls = new System.Windows.Forms.Panel();
+            this.checkBoxReverse = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).BeginInit();
+            this.panelVoiceControls.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonSpeak
+            // 
+            this.buttonSpeak.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSpeak.Location = new System.Drawing.Point(25, 154);
+            this.buttonSpeak.Name = "buttonSpeak";
+            this.buttonSpeak.Size = new System.Drawing.Size(72, 21);
+            this.buttonSpeak.TabIndex = 0;
+            this.buttonSpeak.Text = "Speak";
+            this.buttonSpeak.UseVisualStyleBackColor = true;
+            this.buttonSpeak.Click += new System.EventHandler(this.buttonSpeak_Click);
+            // 
+            // labelVolume
+            // 
+            this.labelVolume.AutoSize = true;
+            this.labelVolume.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelVolume.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelVolume.Location = new System.Drawing.Point(198, 57);
+            this.labelVolume.Name = "labelVolume";
+            this.labelVolume.Size = new System.Drawing.Size(61, 17);
+            this.labelVolume.TabIndex = 0;
+            this.labelVolume.Text = "Volume";
+            this.labelVolume.Click += new System.EventHandler(this.labelVolume_Click);
+            // 
+            // labelSpeed
+            // 
+            this.labelSpeed.AutoSize = true;
+            this.labelSpeed.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSpeed.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelSpeed.Location = new System.Drawing.Point(113, 57);
+            this.labelSpeed.Name = "labelSpeed";
+            this.labelSpeed.Size = new System.Drawing.Size(54, 17);
+            this.labelSpeed.TabIndex = 13;
+            this.labelSpeed.Text = "Speed";
+            // 
+            // trackBarVolume
+            // 
+            this.trackBarVolume.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.trackBarVolume.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trackBarVolume.LargeChange = 10;
+            this.trackBarVolume.Location = new System.Drawing.Point(214, 84);
+            this.trackBarVolume.Maximum = 100;
+            this.trackBarVolume.Name = "trackBarVolume";
+            this.trackBarVolume.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBarVolume.Size = new System.Drawing.Size(45, 155);
+            this.trackBarVolume.SmallChange = 10;
+            this.trackBarVolume.TabIndex = 14;
+            this.trackBarVolume.Value = 100;
+            this.trackBarVolume.Scroll += new System.EventHandler(this.trackBarVolume_Scroll);
+            // 
+            // trackBarSpeed
+            // 
+            this.trackBarSpeed.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trackBarSpeed.LargeChange = 1;
+            this.trackBarSpeed.Location = new System.Drawing.Point(132, 88);
+            this.trackBarSpeed.Maximum = 6;
+            this.trackBarSpeed.Name = "trackBarSpeed";
+            this.trackBarSpeed.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBarSpeed.Size = new System.Drawing.Size(45, 147);
+            this.trackBarSpeed.TabIndex = 15;
+            this.trackBarSpeed.Value = 1;
+            this.trackBarSpeed.Scroll += new System.EventHandler(this.trackBarSpeed_Scroll);
+            // 
+            // radioButtonMale
+            // 
+            this.radioButtonMale.AutoSize = true;
+            this.radioButtonMale.Checked = true;
+            this.radioButtonMale.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonMale.ForeColor = System.Drawing.SystemColors.Info;
+            this.radioButtonMale.Location = new System.Drawing.Point(25, 88);
+            this.radioButtonMale.Name = "radioButtonMale";
+            this.radioButtonMale.Size = new System.Drawing.Size(56, 21);
+            this.radioButtonMale.TabIndex = 16;
+            this.radioButtonMale.TabStop = true;
+            this.radioButtonMale.Text = "Male";
+            this.radioButtonMale.UseVisualStyleBackColor = true;
+            this.radioButtonMale.CheckedChanged += new System.EventHandler(this.radioButtonMale_CheckedChanged);
+            // 
+            // radioButtonFemale
+            // 
+            this.radioButtonFemale.AutoSize = true;
+            this.radioButtonFemale.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonFemale.ForeColor = System.Drawing.SystemColors.Info;
+            this.radioButtonFemale.Location = new System.Drawing.Point(25, 115);
+            this.radioButtonFemale.Name = "radioButtonFemale";
+            this.radioButtonFemale.Size = new System.Drawing.Size(72, 21);
+            this.radioButtonFemale.TabIndex = 17;
+            this.radioButtonFemale.Text = "Female";
+            this.radioButtonFemale.UseVisualStyleBackColor = true;
+            this.radioButtonFemale.CheckedChanged += new System.EventHandler(this.radioButtonFemale_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(22, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Gender";
+            // 
+            // panelVoiceControls
+            // 
+            this.panelVoiceControls.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panelVoiceControls.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelVoiceControls.Controls.Add(this.checkBoxReverse);
+            this.panelVoiceControls.Controls.Add(this.label2);
+            this.panelVoiceControls.Controls.Add(this.buttonStop);
+            this.panelVoiceControls.Controls.Add(this.labelVolume);
+            this.panelVoiceControls.Controls.Add(this.button1);
+            this.panelVoiceControls.Controls.Add(this.radioButtonFemale);
+            this.panelVoiceControls.Controls.Add(this.label1);
+            this.panelVoiceControls.Controls.Add(this.buttonSpeak);
+            this.panelVoiceControls.Controls.Add(this.radioButtonMale);
+            this.panelVoiceControls.Controls.Add(this.trackBarVolume);
+            this.panelVoiceControls.Controls.Add(this.labelSpeed);
+            this.panelVoiceControls.Controls.Add(this.trackBarSpeed);
+            this.panelVoiceControls.Location = new System.Drawing.Point(12, 12);
+            this.panelVoiceControls.Name = "panelVoiceControls";
+            this.panelVoiceControls.Size = new System.Drawing.Size(282, 291);
+            this.panelVoiceControls.TabIndex = 19;
+            // 
+            // checkBoxReverse
+            // 
+            this.checkBoxReverse.AutoSize = true;
+            this.checkBoxReverse.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxReverse.ForeColor = System.Drawing.Color.White;
+            this.checkBoxReverse.Location = new System.Drawing.Point(25, 255);
+            this.checkBoxReverse.Name = "checkBoxReverse";
+            this.checkBoxReverse.Size = new System.Drawing.Size(80, 21);
+            this.checkBoxReverse.TabIndex = 20;
+            this.checkBoxReverse.Text = "Reverse";
+            this.checkBoxReverse.UseVisualStyleBackColor = true;
+            this.checkBoxReverse.CheckedChanged += new System.EventHandler(this.checkBoxReverse_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(75, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 20);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Voice Controls";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStop.Location = new System.Drawing.Point(25, 208);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(72, 21);
+            this.buttonStop.TabIndex = 20;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(25, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 21);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Pause";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonPause_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.textBox1.Location = new System.Drawing.Point(300, 278);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(652, 25);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = " ";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.LimeGreen;
+            this.textBox2.Location = new System.Drawing.Point(300, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(652, 260);
+            this.textBox2.TabIndex = 20;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(964, 316);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.panelVoiceControls);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Blurt";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).EndInit();
+            this.panelVoiceControls.ResumeLayout(false);
+            this.panelVoiceControls.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSpeak;
+        private System.Windows.Forms.Label labelVolume;
+        private System.Windows.Forms.Label labelSpeed;
+        private System.Windows.Forms.TrackBar trackBarVolume;
+        private System.Windows.Forms.TrackBar trackBarSpeed;
+        private System.Windows.Forms.RadioButton radioButtonMale;
+        private System.Windows.Forms.RadioButton radioButtonFemale;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelVoiceControls;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBoxReverse;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace Blurt_Reader
+{
+    public partial class Form1 : Form
+    {
+        private SpeechSynthesizer voiceBox;
+        private string text;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            text = String.Empty;
+
+            voiceBox = new SpeechSynthesizer()
+            {
+                Rate = trackBarSpeed.Value,
+                Volume = trackBarVolume.Value
+            };
+
+            voiceBox.SpeakProgress += OnWordSpoken;
+            voiceBox.SpeakStarted += OnStartedSpeaking;
+        }
+
+        private void OnWordSpoken(object sender, SpeakProgressEventArgs e)
+        {
+            textBox2.AppendText(e.Text + " ");
+        }
+
+        private void OnStartedSpeaking(object sender, SpeakStartedEventArgs e)
+        {
+            textBox2.Text = String.Empty;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelVolume_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSpeak_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != null)
+            {
+                if (voiceBox.State == SynthesizerState.Paused)
+                {
+                    voiceBox.Resume();
+                }
+                //else if (voiceBox.GetCurrentlySpokenPrompt() != null)
+                else if (voiceBox.GetCurrentlySpokenPrompt() == null)
+                {
+                    //voiceBox.SpeakAsyncCancel(voiceBox.GetCurrentlySpokenPrompt());
+                    text = textBox1.Text;
+
+                    if (checkBoxReverse.Checked)
+                    {
+                        char[] charSequence = text.ToCharArray();
+                        text = new string(charSequence.Reverse().ToArray<char>());
+                    }
+
+                    voiceBox.SpeakAsync(text);
+                }
+            }
+        }
+
+        private void buttonPause_Click(object sender, EventArgs e)
+        {
+            if (voiceBox.State == SynthesizerState.Speaking)
+            {
+                voiceBox.Pause();
+            }
+        }
+
+        private void buttonResume_Click(object sender, EventArgs e)
+        {
+            if (voiceBox.State == SynthesizerState.Paused)
+            {
+                voiceBox.Resume();
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if (voiceBox.GetCurrentlySpokenPrompt() != null)
+            {
+                voiceBox.SpeakAsyncCancel(voiceBox.GetCurrentlySpokenPrompt());
+            }
+        }
+
+        private void trackBarVolume_Scroll(object sender, EventArgs e)
+        {
+            voiceBox.Volume = trackBarVolume.Value;
+        }
+
+        private void trackBarSpeed_Scroll(object sender, EventArgs e)
+        {
+            voiceBox.Rate = trackBarSpeed.Value;
+        }
+
+        private void radioButtonFemale_CheckedChanged(object sender, EventArgs e)
+        {
+            voiceBox.SelectVoiceByHints(VoiceGender.Female);
+        }
+
+        private void radioButtonMale_CheckedChanged(object sender, EventArgs e)
+        {
+            voiceBox.SelectVoiceByHints(VoiceGender.Male);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBoxReverse_CheckedChanged(object sender, EventArgs e)
+        {
+            text.Reverse();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
